d4904c1dc4105e96a04ad31dabcaa39277b57071
paedayz <pae16phasit@gmail.com>
2021-01-16 12:14:17 +0700

    created and import user route

---
+++ client/screen/Auth/AuthScreen.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { SafeAreaView, Text } from 'react-native';
+
+export default function AuthScreen({navigation}) {
+    
+      return (
+        <SafeAreaView style={{ flex: 1 }}>
+          <Text>Auth Screen</Text>
+        </SafeAreaView>
+      );
+}
--- server/server.js
+++ server/server.js
@@ -2,16 +2,19 @@ let express = require("express")
 let bodyParser = require("body-parser")
 
 let app = express()
-
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-
 app.set("port", process.env.PORT || 3003);
 
-app.get('/', (req,res) => {
-    console.log('connected')
-    return res.json({data: 'Yes sir'})
-})
+// import functions
+let {signup, login, checkAuthen, signout} = require('./handler/user')
+
+// User Route
+app.post('/signup',signup)
+app.post('/login', login)
+app.get('/authen', checkAuthen)
+app.get('/signout', signout)
+
 
 app.listen(app.get("port"), function () {
     console.log("run at port", app.get("port"));
