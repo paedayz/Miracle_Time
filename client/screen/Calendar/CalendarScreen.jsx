11106d9be577bee8e44cfd31bb4cb41b8268560b
paedayz <pae16phasit@gmail.com>
2021-01-17 19:46:06 +0700

    can use calendar first phase

--- client/screen/Calendar/CalendarScreen.jsx
+++ client/screen/Calendar/CalendarScreen.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import { SafeAreaView, Text, Button } from 'react-native';
 
+// Component
+import Calendar from './Calendar'
+
 export default function CalendarScreen({navigation}) {
     
       return (
         <SafeAreaView style={{ flex: 1 }}>
-          <Text>Calendar Screen</Text>
+          <Calendar Test={[{date:'2021-01-17', name: 'one'},{date:'2021-01-17', name: 'two'},{date:'2021-01-18', name: '2021-01-18'}]} />
         </SafeAreaView>
       );
 }
