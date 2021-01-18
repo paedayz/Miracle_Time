fe14cfc5deb853d4c153b9965349da5b4927bae2
paedayz <pae16phasit@gmail.com>
2021-01-19 04:20:30 +0700

    get All events function

--- server/handler/data.js
+++ server/handler/data.js
@@ -1,9 +1,18 @@
+const FBAuth = require("../util/FBAuth")
 const {firebase, firestore} = require("../util/firebase")
 
 exports.getAllEvents = (req, res) => {
     console.log(req.user.username)
-    let user = firebase.auth().currentUser;
-    if (user != null) {
-        return res.json({uid : user.uid})
-      }
+    firestore.collection('events').where('username', '==', req.user.username).get()
+        .then((snapshot) => {
+            let data = []
+            snapshot.forEach(function(doc){
+                data.push(doc.data())
+            })
+            return res.json({data: data})
+        })
+        .catch((err) => {
+            console.log(err)
+            return res.json({error: err})
+          })
 }
