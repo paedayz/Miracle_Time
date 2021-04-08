9864d7b9a83906e7c8c42b1c32eff8365d843ed6
paedayz <pae16phasit@gmail.com>
2021-04-09 00:34:39 +0700

    [client]: add picture by using photo in moblie

--- client/redux/action/dailyAction.jsx
+++ client/redux/action/dailyAction.jsx
@@ -9,6 +9,9 @@ import {
     LOADING_DAILY,
     SUCCESS_LOADING_DAILY
 } from "../type"
+import firebase from 'firebase'
+
+require('firebase/storage')
 
 import {getClientUserId} from './userAction'
 
@@ -34,19 +37,50 @@ export const getUserDaily = () => (dispatch) => {
         })
 }
 
-export const addDaily = (dailyData) => (dispatch) => {
+export const addDaily = (dailyData, blob) => (dispatch) => {
     dispatch({type: LOADING_DAILY})
     let clientUserId = getClientUserId()
     dailyData.clientUserId = clientUserId
-    axios.post('/addDaily',dailyData)
-        .then((res) => {
-            dispatch({type: ADD_DAILY, payload: res.data.data})
-            dispatch({type:SUCCESS_LOADING_DAILY})
+
+    const imageName = blob._data.name
+    dailyData.image = `https://firebasestorage.googleapis.com/v0/b/dtime-44c09.appspot.com/o/${imageName}?alt=media`
+    dailyData.clientUserId = clientUserId
+
+    const task = firebase.storage().ref().child(imageName).put(blob);
+
+    const taskProgress = snapshot => {
+        console.log('inprogress')
+    }
+
+    const taskCompleted = () => {
+        task.snapshot.ref.getDownloadURL().then((snapshot) => {
+            console.log('success')
+            axios.post('/addDaily',dailyData)
+            .then((res) => {
+                let set_data = res.data.data
+                let day = set_data.createdAt.split(',')[0].split('/')[1]
+                let month = set_data.createdAt.split(',')[0].split('/')[0]
+                let year = set_data.createdAt.split(',')[0].split('/')[2]
+                let time = set_data.createdAt.split(',')[1]
+                set_data.createdAt = `${day}/${month}/${year},${time}`
+                dispatch({type: ADD_DAILY, payload: set_data})
+                dispatch({type:SUCCESS_LOADING_DAILY})
+            })
+            .catch((err) => {
+                console.log(err)
+                dispatch({type:SUCCESS_LOADING_DAILY})
+            })
         })
-        .catch((err) => {
-            console.log(err)
-            dispatch({type:SUCCESS_LOADING_DAILY})
-        })
+        
+    }
+
+    const taskError = snapshot => {
+        console.log('errors')
+    }
+
+    task.on("state_changed", taskProgress, taskError, taskCompleted);
+
+    
 }
 
 export const editDaily= (dailyData,docId) => (dispatch) => {
