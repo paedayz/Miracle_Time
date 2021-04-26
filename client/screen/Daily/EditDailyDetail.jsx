1769e23a239de77bed640f4c5ae4d6b90a0c81b7
nattapat.v <65421788+nattapat2543@users.noreply.github.com>
2021-04-27 02:15:53 +0700

    Dairy - move cucur to top in textinput

--- client/screen/Daily/EditDailyDetail.jsx
+++ client/screen/Daily/EditDailyDetail.jsx
@@ -2,8 +2,8 @@ import React, {useState, useEffect} from 'react'
 import { StyleSheet, Text, View, Button, LogBox , Platform ,TouchableOpacity, Image, ScrollView} from 'react-native'
 import { Formik } from 'formik'
 import { TextInput } from 'react-native-gesture-handler'
-import RNPickerSelect from 'react-native-picker-select';
-import * as ImagePicker from 'expo-image-picker';
+import RNPickerSelect from 'react-native-picker-select'
+import * as ImagePicker from 'expo-image-picker'
 
 //redux
 import {useDispatch} from 'react-redux'
@@ -152,7 +152,8 @@ const styles = StyleSheet.create({
         fontSize: 14,
         borderRadius: 6,
         marginBottom:5,
-        marginTop:10
+        marginTop:10,
+        textAlignVertical: 'top'
     },
     container:{
         flex: 1,
